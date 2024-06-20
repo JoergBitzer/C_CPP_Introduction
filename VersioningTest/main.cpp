21a41b74a8f144b6ffd16646c74a8c131669fcbd
Joerg Bitzer <joerg.bitzer@jade-hs.de>
2024-06-20 15:42:35 +0200

    versioning added

---
+++ VersioningTest/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "version.h"
+//#define VERSION_MAJOR 0
+//#define VERSION_MINOR 1
+//#define VERSION_PATCH 0
+
+int main()
+{
+    std::cout << "Version: " << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << std::endl;
+    return 0;
+}
+
+
